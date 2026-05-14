0080c41e26ff1a0610c4d9d1a05c9a804c64dec8
Gerard Fernandez <gerard.fernandez@col.vueling.com>
2026-05-14 14:05:39 +0200

    refactor: remove mobile navigation component and related files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,11 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { NavbarComponent } from './shared/components/navbar/navbar.component';
 import { FooterComponent } from './shared/components/footer/footer.component';
-import { MobileNavComponent } from './shared/components/mobile-nav/mobile-nav.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, FooterComponent, MobileNavComponent],
+  imports: [RouterOutlet, NavbarComponent, FooterComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
